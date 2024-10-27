eb49eb2f23e13234e0dc87ab0413b26f7521dd92
Hu Ji <669898595@qq.com>
2024-10-27 13:54:43 +0800

    add ws-scrcpy

---
+++ 605-ws-scrcpy/container.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author  : Hu Ji
+@file    : deploy.py 
+@time    : 2023/05/21
+@site    :  
+@software: PyCharm 
+
+              ,----------------,              ,---------,
+         ,-----------------------,          ,"        ,"|
+       ,"                      ,"|        ,"        ,"  |
+      +-----------------------+  |      ,"        ,"    |
+      |  .-----------------.  |  |     +---------+      |
+      |  |                 |  |  |     | -==----'|      |
+      |  | $ sudo rm -rf / |  |  |     |         |      |
+      |  |                 |  |  |/----|`---=    |      |
+      |  |                 |  |  |   ,/|==== ooo |      ;
+      |  |                 |  |  |  // |(((( [33]|    ,"
+      |  `-----------------'  |," .;'| |((((     |  ,"
+      +-----------------------+  ;;  | |         |,"
+         /_)______________(_/  //'   | +---------+
+    ___________________________/___  `,
+   /  oooooooooooooooo  .o.  oooo /,   `,"-----------
+  / ==ooooooooooooooo==.o.  ooo= //   ,``--{)B     ,"
+ /_==__==========__==_ooo__ooo=_/'   /___________,"
+"""
+import os.path
+import shutil
+import zipfile
+
+import yaml
+from linktools import Config, utils
+from linktools.container import BaseContainer, ExposeLink
+from linktools.decorator import cached_property
+
+
+class Container(BaseContainer):
+
+    @cached_property
+    def configs(self):
+        return dict(
+            WS_SCRCPY_TAG="v0.8.1",
+            WS_SCRCPY_PORT=Config.Alias(default=8000, type=int),
+        )
+
+    @cached_property
+    def exposes(self) -> [ExposeLink]:
+        return [
+            self.expose_container(
+                "ws-scrcpy", "cellphone", "ws-scrcpy",
+                self.load_port_url("WS_SCRCPY_PORT", https=False)),
+        ]
+
+    @cached_property
+    def source_path(self):
+        tag = self.manager.config.get("WS_SCRCPY_TAG")
+
+        zip_path = self.get_app_path(f"ws-scrcpy-{tag}.zip")
+        source_path = str(zip_path) + ".unzip"
+
+        def init_source_code():
+            if not os.path.isdir(source_path):
+                file = self.manager.environ.get_url_file(
+                    f"https://github.com/NetrisTV/ws-scrcpy/archive/refs/tags/{tag}.zip")
+                file.save(zip_path.parent, zip_path.name)
+                os.makedirs(source_path, exist_ok=True)
+                try:
+                    with zipfile.ZipFile(zip_path) as f:
+                        for names in f.namelist():
+                            f.extract(names, source_path)
+                except:
+                    utils.ignore_error(os.remove, args=(zip_path,))
+                    shutil.rmtree(source_path, ignore_errors=True)
+                    raise
+
+        self.start_hooks.append(init_source_code)
+        return os.path.join(source_path, f"ws-scrcpy-{tag.lstrip('v')}")
+
+    def on_starting(self):
+        with open(self.get_app_path("config.yaml"), "wt") as fd:
+            yaml.dump({
+                "server": [{
+                    "secure": False,
+                    "port": self.manager.config.get("WS_SCRCPY_PORT")
+                }]
+            }, fd)
--- 610-redroid/container.py
+++ 610-redroid/container.py
@@ -43,6 +43,9 @@ class Container(BaseContainer):
         return dict(
             REDROID_IMAGE="iceblacktea/redroid-arm64:12.0.0-240328",
             REDROID_COUNT=Config.Prompt(default=3, type=int, cached=True),
+            REDROID_WIDTH=Config.Alias(default=720, type=int),
+            REDROID_HEIGHT=Config.Alias(default=1280, type=int),
+            REDROID_DPI=Config.Alias(default=320, type=int),
             REDROID_ADB_PORT=Config.Prompt(default=5555, type=int, cached=True),
             REDROID_GPU_MODE=Config.Prompt(default="mali", type=str, choices=["auto", "host", "guest", "mali"], cached=True),
             REDROID_VIRTUAL_WIFI=Config.Confirm(default=True, cached=True),
